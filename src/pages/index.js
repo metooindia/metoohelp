a451aee7f93366664ecaf1f88ab457051f5edb63
Tanvi Bhakta <tanvibhakta@gmail.com>
2018-10-12 12:27:47 +0530

    Initial commit with gatsby, a few plugins, and custom text

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello, India</h1>
+    <p>This site will be a repository of resources you need post #MeToo in India.</p>
+    <p>If you want to help with curating legal and mental health resources for survivors, please reach out to @IndiaMeToo or @padawanjack on twitter.</p>
+  </Layout>
+)
+
+export default IndexPage
